f0e6b6fe25a2df8332762f9c3c8c967da2e88a21
Gurpreet Bhopal <gurpreet.bhopal@res-group.com>
2023-02-07 11:32:15 +0000

    Add Overflow Hidden to body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className={"h-full"}>
       <Head />
-      <body className={"h-full bg-black"}>
+      <body className={"h-full bg-black overflow-x-hidden"}>
         <Main />
         <NextScript />
       </body>
