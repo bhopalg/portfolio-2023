d8b34209c4f77ea5211a9c43a12db0abe20f24e3
Gurpreet Bhopal <gbhopal91@gmail.com>
2023-02-08 14:33:42 +0000

    Fix some styling issues on articles

--- components/card.tsx
+++ components/card.tsx
@@ -61,7 +61,7 @@ Card.Description = function CardDescription({ children }: { children: React.Reac
 
 Card.Cta = function CardCta({ children }: { children: React.ReactNode }) {
   return (
-    <div aria-hidden="true" className="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">
+    <div aria-hidden="true" className="relative z-10 mt-4 flex items-center text-sm font-medium text-light-blue-500">
       {children}
       <ChevronRightIcon className="ml-1 h-4 w-4 stroke-current" />
     </div>
--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -23,20 +23,18 @@ function Article({ article }: { article: ArticleModel }) {
   );
 }
 
+const DESCRIPTION =
+  "All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.";
+const TITLE = "Writing on software design and company building.";
+
 export default function ArticlesIndex({ articles }: { articles: ArticleModel[] }) {
   return (
     <>
       <Head>
         <title>Articles - Spencer Sharp</title>
-        <meta
-          name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-        />
+        <meta name="description" content={DESCRIPTION} />
       </Head>
-      <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-      >
+      <SimpleLayout title={TITLE} intro={DESCRIPTION}>
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
