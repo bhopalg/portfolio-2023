efab9f0777e42c9a93754488e5fecd9619bb3d38
Gurpreet Bhopal <gbhopal91@gmail.com>
2023-02-08 15:34:15 +0000

    updated resume card

--- common/constants.ts
+++ common/constants.ts
@@ -13,4 +13,38 @@ export const NAVIGATION = [
   },
 ];
 
-export const EXPERIENCE_START_YEAR = 2015;
+export const EXPERIENCE_START_YEAR = 2014;
+
+export const RESUME = [
+  {
+    company: "RES",
+    title: "Software Engineer",
+    logo: "res-group.svg",
+    start: "2022",
+    end: {
+      label: "Present",
+      dateTime: new Date().getFullYear(),
+    },
+  },
+  {
+    company: "Smarter Data Solutions",
+    title: "Senior Software Engineer",
+    logo: "/sdm-logo.jpeg",
+    start: "2020",
+    end: "2022",
+  },
+  {
+    company: "Smarter Data Solutions",
+    title: "Software Engineer",
+    logo: "/sdm-logo.jpeg",
+    start: "2016",
+    end: "2020",
+  },
+  {
+    company: "Sports Direct",
+    title: "Web Designer",
+    logo: "sports-direct.svg",
+    start: "2014",
+    end: "2015",
+  },
+];
--- components/resume.tsx
+++ components/resume.tsx
@@ -1,52 +1,19 @@
+import { RESUME } from "@/common/constants";
 import { BriefcaseIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 export function Resume() {
-  const resume = [
-    {
-      company: "Planetaria",
-      title: "CEO",
-      logo: "planetaria.svg",
-      start: "2019",
-      end: {
-        label: "Present",
-        dateTime: new Date().getFullYear(),
-      },
-    },
-    {
-      company: "Airbnb",
-      title: "Product Designer",
-      logo: "airbnb.svg",
-      start: "2014",
-      end: "2019",
-    },
-    {
-      company: "Facebook",
-      title: "iOS Software Engineer",
-      logo: "facebook.svg",
-      start: "2011",
-      end: "2014",
-    },
-    {
-      company: "Starbucks",
-      title: "Shift Supervisor",
-      logo: "starbucks.svg",
-      start: "2008",
-      end: "2011",
-    },
-  ];
-
   return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+    <div className="rounded-2xl bg-zinc-900 ring-1 ring-zinc-300/20 p-6">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
         <span className="ml-3">Work</span>
       </h2>
       <ol className="mt-6 space-y-4">
-        {resume.map(({ logo, company, start, title, end }, roleIndex) => (
+        {RESUME.map(({ logo, company, start, title, end }, roleIndex) => (
           <li key={roleIndex} className="flex gap-4">
             <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image src={logo} alt={company} width={28} height={28} />
+              <Image src={logo} alt={company} width={28} height={28} className={"rounded-full"} />
             </div>
             <dl className="flex flex-auto flex-wrap gap-x-2">
               <dt className="sr-only">Company</dt>
