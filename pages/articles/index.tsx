819c89d93a7bc0bcf2247f801ab10c78681c1ff3
Gurpreet Bhopal <gbhopal91@gmail.com>
2023-02-12 17:15:50 +0000

    styling updates

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -35,7 +35,7 @@ export default function ArticlesIndex({ articles }: { articles: ArticleModel[] }
         <meta name="description" content={DESCRIPTION} />
       </Head>
       <SimpleLayout title={TITLE} intro={DESCRIPTION}>
-        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="md:border-l md:pl-6 md:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
