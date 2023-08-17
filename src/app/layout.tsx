457b3b7168cd729b1cc7ef587c08ac96b8207809
Gurpreet Bhopal <gurpreet.bhopal@res-group.com>
2023-08-17 21:12:57 +0100

    Add metadata support for Next.js
    Add metadata support for Next.js layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,58 @@ import "@/styles/globals.css";
 import "@code-hike/mdx/dist/index.css";
 import { Roboto_Mono } from "next/font/google";
 import BaseLayout from "@/components/BaseLayout";
+import { Metadata } from "next";
 
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Gurp's Portfolio",
+  description:
+    "Explore my portfolio website showcasing my skills and experience in web development. I'm a full-stack developer proficient in web technologies like React, Node.js, Express, and MongoDB. Check out my projects, resume, and contact information",
+  keywords: [
+    "Portfolio website",
+    "Web development",
+    "Front-end development",
+    "Back-end development",
+    "Full-stack development",
+    "Projects",
+    "Skills",
+    "Experience",
+    "Resume",
+    "About me",
+    "Contact",
+    "UI/UX",
+    "Responsive design",
+    "Web accessibility",
+    "SEO optimization",
+    "Code snippets",
+    "Web technologies",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "REST API",
+    "GraphQL",
+    "Git",
+    "Agile methodology",
+  ],
+  twitter: {
+    card: "summary",
+    title: "Gurp's Portfolio",
+    description:
+      "Explore my portfolio website showcasing my skills and experience in web development. I'm a full-stack developer proficient in web technologies like React, Node.js, Express, and MongoDB. Check out my projects, resume, and contact information",
+  },
+  openGraph: {
+    title: "Gurp's Portfolio",
+    description:
+      "Explore my portfolio website showcasing my skills and experience in web development. I'm a full-stack developer proficient in web technologies like React, Node.js, Express, and MongoDB. Check out my projects, resume, and contact information",
+
+    url: "https://gbhopal.com",
+  },
+};
 
 export default function RootLayout({
   // Layouts must accept a children prop.
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { HackerText } from "@/components/HackerText";
+import Logo from "@/components/Logo";
 
 export function Hero() {
   return (
-    <div className="lg:min-h-[calc(100vh_-_190px)] flex justify-center items-center mx-auto md:max-w-7xl w-full hero-text-typer">
+    <div className="lg:min-h-[calc(100vh_-_190px)] flex justify-center items-center mx-auto md:max-w-7xl w-full hero-text-typer flex-col gap-5">
+      <Logo className="h-20 w-20" />
       <HackerText />
     </div>
   );
