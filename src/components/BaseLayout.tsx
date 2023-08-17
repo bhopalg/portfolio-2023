a15cc532ee009bb0035b0702c62f0e2042eb286a
Gurpreet Bhopal <gurpreet.bhopal@res-group.com>
2023-08-17 15:23:44 +0100

    Removed how-to-transfer-domains-from-godaddy-to-aws-route-53 index.mdx
    Remove how-to-transfer-domains-from-godaddy-to-aws-route-53 index.mdx file.

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -3,15 +3,12 @@
 import { ReactNode } from "react";
 import { Header } from "@/components/header/header";
 import { Footer } from "@/components/footer";
-import { MDXProvider } from "@mdx-js/react";
 
 export default function BaseLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
-      <main>
-        <MDXProvider>{children}</MDXProvider>
-      </main>
+      <main>{children}</main>
       <Footer />
     </>
   );
