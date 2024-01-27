2e409baeb1ac3abf88a0c9576a65e342b1aeee24
Gurpreet Bhopal <gbhopal91@gmail.com>
2024-01-27 18:49:03 +0000

    Refactor Hero component - remove unused code
    Remove unused code in Hero component for cleaner code structure.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,6 @@
 export function Hero() {
   return (
     <div className="grow flex justify-center items-center mx-auto w-full hero-text-typer flex-col gap-5 px-5 relative">
-      <HeroVideo />
       <div className="absolute top-0 left-0 w-full h-full z-30 flex flex-col justify-center items-center mix-blend-difference">
         <h1 className="text-white text-5xl md:text-7xl font-bold text-center">Hi, I&apos;m Gurps</h1>
         <h2 className="text-white text-2xl md:text-4xl font-bold text-center">I&apos;m a full-stack web developer</h2>
