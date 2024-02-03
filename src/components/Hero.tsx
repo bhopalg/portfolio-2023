afd3fca197debd57975b60bdbd9718f9b29690df
Gurpreet Bhopal <gbhopal91@gmail.com>
2024-02-03 16:00:02 +0000

    Add HeroVideo component
    Add HeroVideo component to display video in the Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,15 +3,15 @@
 export function Hero() {
   return (
     <div className="grow flex justify-center items-center mx-auto w-full hero-text-typer flex-col gap-5 px-5 relative">
+      <HeroVideo/>
       <div className="absolute top-0 left-0 w-full h-full z-30 flex flex-col justify-center items-center mix-blend-difference">
         <h1 className="text-white text-5xl md:text-7xl font-bold text-center">Hi, I&apos;m Gurps</h1>
-        <h2 className="text-white text-2xl md:text-4xl font-bold text-center">I&apos;m a full-stack web developer</h2>
+        <h2 className="text-white text-2xl md:text-4xl font-bold text-center">I&apos;m a full-stack developer</h2>
       </div>
     </div>
   );
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function HeroVideo() {
   return (
     <video
@@ -23,7 +23,7 @@ function HeroVideo() {
       poster="/wave-poster.jpg"
     >
       <source
-        src="https://gurps-portfolio-assets-public.s3.eu-west-2.amazonaws.com/pexels_videos_1777359.mp4"
+        src="https://tdgm6zgrnrffbtpu.public.blob.vercel-storage.com/pexels_videos_1777359-zA5dOSTczzgIeN7ZPMPATilH2neIgA.mp4"
         type="video/mp4"
       />
     </video>
