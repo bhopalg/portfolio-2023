77ec4f1f84d168d417ed98b2c622024040ded2cc
Gurpreet Bhopal <gbhopal91@gmail.com>
2024-02-04 14:42:52 +0000

    Refactor Experience component
    Improve readability and maintainability of the Experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,29 +1,59 @@
 "use client";
 
 import { EXPERIENCE_START_YEAR } from "@/common/constants";
-import { useMemo } from "react";
+import { forwardRef, useMemo } from "react";
 import Interests from "@/components/Interests";
 import { AboutMeItem, Description, Heading, Title } from "@/components/about-me/ui";
+import * as React from "react";
+import { motion } from "framer-motion";
 
-export default function Experience() {
+export interface ExperienceProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Experience = forwardRef<HTMLDivElement, ExperienceProps>((props, ref) => {
   const yearsOfExperience = useMemo(() => {
     return new Date().getFullYear() - EXPERIENCE_START_YEAR;
   }, []);
 
   return (
     <>
-      <AboutMeItem>
-        <span className="sr-only">{yearsOfExperience} years of experience</span>
-        <Title>{yearsOfExperience} Years</Title>
-        <Description>Experience</Description>
-      </AboutMeItem>
-      <AboutMeItem>
-        <span className="sr-only">Expertise in building data analysis tools and apps for a large user base.</span>
-        <Heading>Expertise in Building</Heading>
-        <Title className="px-5 text-center">Data analysis tools and apps</Title>
-        <Description>for a large user base.</Description>
-      </AboutMeItem>
-      <Interests />
+      <motion.div ref={ref} variants={variants}>
+        <AboutMeItem>
+          <span className="sr-only">{yearsOfExperience} years of experience</span>
+          <Title>{yearsOfExperience} Years</Title>
+          <Description>Experience</Description>
+        </AboutMeItem>
+      </motion.div>
+      <motion.div ref={ref} variants={variants}>
+        <AboutMeItem ref={ref}>
+          <span className="sr-only">Expertise in building data analysis tools and apps for a large user base.</span>
+          <Heading>Expertise in Building</Heading>
+          <Title className="px-5 text-center">Data analysis tools and apps</Title>
+          <Description>for a large user base.</Description>
+        </AboutMeItem>
+      </motion.div>
+      <motion.div ref={ref} variants={variants}>
+        <Interests />
+      </motion.div>
     </>
   );
-}
+});
+Experience.displayName = "Experience";
+
+export default Experience;
+
+const variants = {
+  visible: {
+    opacity: 1,
+    translateX: 0,
+    transition: {
+      type: "spring",
+    },
+  },
+  hidden: {
+    opacity: 0,
+    translateX: -25,
+    transition: {
+      type: "spring",
+    },
+  },
+};
--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -3,34 +3,129 @@ import Frameworks from "@/components/Frameworks";
 import Resume from "@/components/Resume";
 import Socials from "@/components/Socials";
 import { AboutMeColumn, AboutMeGrid, AboutMeItem, Title } from "./ui";
+import { useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 export default function AboutMe() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
   return (
-    <section id="about-me" className="relative grow flex items-center overflow-x-hidden p-10">
+    <motion.section
+      id="about-me"
+      className="relative grow flex items-center overflow-x-hidden p-10"
+      ref={ref}
+      animate={controls}
+      variants={aboutMeVariants}
+      initial="hidden"
+    >
       <AboutMeGrid>
         <AboutMeColumn>
-          <Experience />
+          <Experience ref={ref} />
         </AboutMeColumn>
         <AboutMeColumn className="justify-between">
-          <Frameworks />
+          <motion.div variants={variantYNegative}>
+            <Frameworks />
+          </motion.div>
           <AboutMeItem className="py-8">
             <span className="sr-only">About Me</span>
             <Title size="large">About Me</Title>
           </AboutMeItem>
-          <Resume />
+          <motion.div variants={variantYPostive}>
+            <Resume />
+          </motion.div>
         </AboutMeColumn>
         <AboutMeColumn className="justify-between">
-          <AboutMeItem className="py-8">
-            <span className="sr-only">Website</span>
-            <Title>Websites</Title>
-          </AboutMeItem>
-          <AboutMeItem className="py-8">
-            <span className="sr-only">Apps</span>
-            <Title>Apps</Title>
-          </AboutMeItem>
-          <Socials />
+          <motion.div variants={variantXPositive}>
+            <AboutMeItem className="py-8">
+              <span className="sr-only">Website</span>
+              <Title>Websites</Title>
+            </AboutMeItem>
+          </motion.div>
+          <motion.div variants={variantXPositive}>
+            <AboutMeItem className="py-8">
+              <span className="sr-only">Apps</span>
+              <Title>Apps</Title>
+            </AboutMeItem>
+          </motion.div>
+          <motion.div variants={variantXPositive}>
+            <Socials />
+          </motion.div>
         </AboutMeColumn>
       </AboutMeGrid>
-    </section>
+    </motion.section>
   );
 }
+
+export const aboutMeVariants = {
+  visible: {
+    opacity: 1,
+    transition: {
+      type: "spring",
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      type: "spring",
+    },
+  },
+};
+
+const variantXPositive = {
+  visible: {
+    opacity: 1,
+    translateX: 0,
+    transition: {
+      type: "spring",
+    },
+  },
+  hidden: {
+    opacity: 0,
+    translateX: 25,
+    transition: {
+      type: "spring",
+    },
+  },
+};
+
+const variantYNegative = {
+  visible: {
+    opacity: 1,
+    translateY: 0,
+    transition: {
+      type: "spring",
+    },
+  },
+  hidden: {
+    opacity: 0,
+    translateY: -25,
+    transition: {
+      type: "spring",
+    },
+  },
+};
+
+const variantYPostive = {
+  visible: {
+    opacity: 1,
+    translateY: 0,
+    transition: {
+      type: "spring",
+    },
+  },
+  hidden: {
+    opacity: 0,
+    translateY: 25,
+    transition: {
+      type: "spring",
+    },
+  },
+};
