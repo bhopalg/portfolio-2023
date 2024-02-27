42954a8a5a162092fdc0b18491d6000be6c038bc
Gurpreet Bhopal <gbhopal91@gmail.com>
2024-02-27 14:47:45 +0000

    Update packages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // this will check Typescript files
-  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+  "**/*.(ts|tsx)": () => "pnpm tsc --noEmit",
 
   // This will lint and format TypeScript and
   // JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn eslint --fix ${filenames.join(" ")}`,
-    `yarn prettier --write ${filenames.join(" ")}`,
+    `pnpm eslint --fix ${filenames.join(" ")}`,
+    `pnpm prettier --write ${filenames.join(" ")}`,
   ],
 
   // this will Format MarkDown and JSON
-  "**/*.(md|json)": (filenames) => `yarn prettier --write ${filenames.join(" ")}`,
+  "**/*.(md|json)": (filenames) => `pnpm prettier --write ${filenames.join(" ")}`,
 };
