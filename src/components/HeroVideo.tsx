f13a637e472185c227b086b875a4240aa5f947e8
Gurpreet Bhopal <gbhopal91@gmail.com>
2024-03-20 15:11:11 +0000

    Updated packages

--- src/components/HeroVideo.tsx
+++ src/components/HeroVideo.tsx
@@ -4,7 +4,7 @@ export default function HeroVideo() {
       autoPlay
       loop
       muted
-      className="absolute z-10 w-auto min-w-full max-h-[calc(100dvh_-_80px_-_173px)] lg:max-h-[calc(100dvh_-_80px_-_85px)] max-w-none"
+      className="absolute z-10 w-auto min-w-full max-h-[calc(100dvh_-_80px_-_173px)] lg:max-h-[calc(100dvh_-_80px_-_85px)] max-w-none object-fill"
     >
       <source src="https://d36qya9vwg8890.cloudfront.net/space-video.mp4" type="video/mp4" />
       Your browser does not support the video tag.
